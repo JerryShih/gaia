f054576bd97aadb2fd4e0441ddb16ebeae25d71b
Jerry Shih <hshih@mozilla.com>
2014-11-25 20:40:49 +0800

    webgl cube showcase

---
+++ dev_apps/webgl-cube/cube_fvc.js
@@ -0,0 +1,79 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "generatedBy"   : "Blender 2.63 Exporter",
+        "vertices"      : 8,
+        "faces"         : 6,
+        "normals"       : 0,
+        "colors"        : 22,
+        "uvs"           : 0,
+        "materials"     : 1,
+        "morphTargets"  : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Material",
+	"blending" : "NormalBlending",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.800000011920929, 0.800000011920929, 0.800000011920929],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"depthTest" : true,
+	"depthWrite" : true,
+	"shading" : "Lambert",
+	"specularCoef" : 50,
+	"transparency" : 1.0,
+	"transparent" : false,
+	"vertexColors" : true
+	}],
+
+    "vertices": [1.0,-1.0,-1.0,
+                 1.0,-1.0,1.0,
+                 -1.0,-1.0,1.0,
+                 -1.0,-1.0,-1.0,
+                 1.0,1.0,-1.0,
+                 1.0,1.0,1.0,
+                 -1.0,1.0,1.0,
+                 -1.0,1.0,-1.0],
+
+    "morphTargets": [],
+
+    "normals": [],
+
+    "colors": [
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255,
+               65280,
+               16711680,
+               255
+                 ],
+
+    "uvs": [[]],
+
+    "faces": [131,0,1,2,3,0,0,1,2,3,131,4,7,6,5,0,4,5,6,7,131,0,4,5,1,0,0,8,9,10,131,1,5,6,2,0,0,11,12,13,131,2,6,7,3,0,14,15,16,17,131,4,0,3,7,0,18,19,20,21]
+
+
+
+}
